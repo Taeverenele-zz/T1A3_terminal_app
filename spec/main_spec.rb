b0d987d6f55504bb883c247c138246ea3ae7c4e1
Nele Taevere <ntaevere@gmail.com>
2020-09-25 09:00:40 +1000

    add second test to User class

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -7,4 +7,10 @@ describe User do
         user = User.new(name, password)
         expect(user.name).to eq(name)
     end
+    it 'should return the password of the user' do
+        name = 'Nele'
+        password = '123password'
+        user = User.new(name, password)
+        expect(user.password).to eq(password)
+    end
 end

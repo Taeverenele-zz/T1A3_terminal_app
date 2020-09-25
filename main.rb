5bcf56754f80c25a9b6fd24e578c935003a677e5
Nele Taevere <ntaevere@gmail.com>
2020-09-25 13:19:27 +1000

    move alreadyUser? and checkIfNameExists methods into the User class

--- main.rb
+++ main.rb
@@ -3,80 +3,54 @@ require 'tty-prompt'
 require 'json'
 require_relative './user'
 
-def welcomeMsg
-    welcome = Artii::Base.new
-    puts welcome.asciify('HTML Boilerpate Creator')
-    puts "***********************************************************************************************************************"
-    puts "Welcome to the HTML Boilerplate creator,"
-    puts  "where we will help you set up an HTML file specific to your project needs!"
-    puts
-    puts "If you would like to learn more about this app before continuing, "
-    puts "please type 'help' otherwise type 'start' to start creating your first boilerplate!"
-    # user_input = gets.chomp
-    #     if user_input == "help"
-    #         puts "More infor about the app"
+# app_on = true
 
-    #     else user_input == "start"
-    #         puts "Ok lets get going"
-    #     end
+    def welcomeMsg
+        welcome = Artii::Base.new
+        puts welcome.asciify('HTML Boilerpate Creator')
+        puts "***********************************************************************************************************************"
+        puts "Welcome to the HTML Boilerplate creator,"
+        puts  "where we will help you set up an HTML file specific to your project needs!"
+        puts
+        puts "If you would like to learn more about this app before continuing, "
+        puts "please type 'help' otherwise type 'start' to start creating your first boilerplate!"
+        # user_input = gets.chomp
+        #     if user_input == "help"
+        #         puts "More infor about the app"
 
-end
+        #     else user_input == "start"
+        #         puts "Ok lets get going"
+        #     end
 
-
-users_array = [
-    {name: 'Nele',
-    password: '123456'},
-    {name: 'John',
-    password: 'qwerty'}
-]
-system 'clear'
-welcomeMsg
-users_array.each do |user|
-    puts user[:name]
-end
-def alreadyUser?(arr, user_name)
-    arr.each do |user|
-        if user[:name] == user_name
-            puts "Please enter your password: "
-            user_password = gets.chomp
-            if user[:password] == user_password
-                puts "Login successful"
-                exit
-            else
-                puts "Incorrect password"
-                exit
-            end
-        else
-            puts "Cannot find username in system"
-        end
     end
-end
 
-def checkIfNameExists(arr, user_name)
-    arr.each do |user|
-        loop do 
-            if user[:name] == user_name
-                puts "Sorry, this username is taken, please choose another one: "
-                user_name = gets.chomp
-            end
-            break if user[:name] != user_name
-        end
-        puts "Please enter a password"
-        user_password = gets.chomp
-        exit
+
+    users_array = [
+        {name: 'Nele',
+        password: '123456'},
+        {name: 'John',
+        password: 'qwerty'}
+    ]
+
+    # while app_on
+
+    system 'clear'
+    welcomeMsg
+
+    puts "Are you already a member? (yes/no)"
+    user_input = gets.chomp
+    if user_input == 'yes'
+        puts "Please enter your name: "
+        user_name = gets.chomp
+        User.alreadyUser?(users_array, user_name)
+    else
+        puts "Please enter a username: "
+        user_name = gets.chomp
+        User.checkIfNameExists(users_array, user_name)
     end
-end
-puts "Are you already a member? (yes/no)"
-user_input = gets.chomp
-if user_input == 'yes'
-    puts "Please enter your name: "
-    user_name = gets.chomp
-    alreadyUser?(users_array, user_name)
-else
-    puts "Please enter a username: "
-    user_name = gets.chomp
-    checkIfNameExists(users_array, user_name)
-end
+    puts users_array
+
+# end
 
 
 
--- user.rb
+++ user.rb
@@ -4,5 +4,38 @@ class User
         @user_name = user_name
         @user_password = user_password
     end
+    def self.alreadyUser?(arr, user_name)
+        arr.each do |user|
+            if user[:name] == user_name
+                puts "Please enter your password: "
+                user_password = gets.chomp
+                if user[:password] == user_password
+                    puts "Login successful"
+                    exit
+                else
+                    puts "Incorrect password"
+                end
+            else
+                puts "Cannot find username in system"
+            end
+        end
+    end
 
+    def self.checkIfNameExists(arr, user_name)
+        newUserHash = {}
+        arr.each do |user|
+            loop do 
+                if user[:name] == user_name
+                    puts "Sorry, this username is taken, please choose another one: "
+                    user_name = gets.chomp
+                end
+                break if user[:name] != user_name
+            end
+            puts "Please enter a password"
+            user_password = gets.chomp
+            newUserHash[:name] = user_name
+            newUserHash[:password] = user_password
+            exit
+        end
+    end
 end

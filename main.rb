f6665a0cdbbce21afc4b420a46a8c9b12148db36
Nele Taevere <ntaevere@gmail.com>
2020-09-26 12:43:06 +1000

    create a command to create a new folder

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'artii'
 require 'tty-prompt'
 require 'json'
 require_relative './user'
+require_relative './methods/methods'
 
 app_on = true
 
@@ -18,48 +19,68 @@ end
 
 while app_on
 
-    system 'clear'
-    welcomeMsg
     prompt = TTY::Prompt.new(symbols: {marker: '►'})
     user_menu_input = prompt.select("Main Menu") do |menu|
-        menu.choice 'Sign In', 1
-        menu.choice 'Sign Up', 2
-        menu.choice 'Help', 3
+        menu.choice 'Start', 1
         menu.choice 'Exit', 4
     end
+
     case user_menu_input
     when 1
-        attempts = 1
-        while attempts < 4
-            print "Username: "
-            username = gets.chomp
-            print "Password: "
-            password = gets.chomp
-            puts User.authenticateUser(username, password, users_array)
-
-            puts "Press n to quit or any other key to continue: "
-            input = gets.chomp.downcase
-            break if input == "n"
-            attempts += 1
-        end
-        puts "You have exceeded the number of attempts" if attempts == 4
-    when 2
-        puts "Please enter a username: "
-        user_name = gets.chomp
-        puts User.checkIfNameExists(users_array, user_name)
-        newUser = {}
-        newUser[:name] = user_name
-        puts "Please enter a password: "
-        password = gets.chomp
-        newUser[:password] = password
-        users_array << newUser
-        File.open("data.json", "w") do |f|
-            f.write(users_array.to_json)
-        end
-    when 3
-        puts "More information about this app"
+        print "Name your project folder: "
+        folder_name = gets.chomp
+        system("mkdir #{folder_name}")
     when 4
         system("clear")
         app_on = false
     end
 end
+
+
+# while app_on
+
+#     system 'clear'
+#     welcomeMsg
+#     prompt = TTY::Prompt.new(symbols: {marker: '►'})
+#     user_menu_input = prompt.select("Main Menu") do |menu|
+#         menu.choice 'Sign In', 1
+#         menu.choice 'Sign Up', 2
+#         menu.choice 'Help', 3
+#         menu.choice 'Exit', 4
+#     end
+#     case user_menu_input
+#     when 1
+#         attempts = 1
+#         while attempts < 4
+#             print "Username: "
+#             username = gets.chomp
+#             print "Password: "
+#             password = gets.chomp
+#             puts User.authenticateUser(username, password, users_array)
+
+#             puts "Press n to quit or any other key to continue: "
+#             input = gets.chomp.downcase
+#             break if input == "n"
+#             attempts += 1
+#         end
+#         puts "You have exceeded the number of attempts" if attempts == 4
+#     when 2
+#         puts "Please enter a username: "
+#         user_name = gets.chomp
+#         puts User.checkIfNameExists(users_array, user_name)
+#         newUser = {}
+#         newUser[:name] = user_name
+#         puts "Please enter a password: "
+#         password = gets.chomp
+#         newUser[:password] = password
+#         users_array << newUser
+#         File.open("data.json", "w") do |f|
+#             f.write(users_array.to_json)
+#         end
+#     when 3
+#         puts "More information about this app"
+#     when 4
+#         system("clear")
+#         app_on = false
+#     end
+# end
---
+++ methods/methods.rb
@@ -0,0 +1,11 @@
+def createFolder(folder_name)
+    system("mkdir folder_name")
+end
+
+def createFile(file_name)
+    system("touch file_name")
+end
+
+def moveFile(file_name, folder)
+    system("file_name folder")
+end
